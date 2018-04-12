0aa342727b34405bed19d8cb4972e18a330924ec
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-13 00:54:43 +0530

    graph.py: Fix errors in module

--- lib/graph.py
+++ lib/graph.py
@@ -8,16 +8,16 @@ class DirGraph:
     v        stores number of vertices in the graph
     e         stores number of edges in the graph
     """
-    def __init__(vertices):
+    def __init__(self, vertices):
         self.v = vertices
-        self.adj = [[] for i in range(v)]
+        self.adj = [[] for i in range(self.v)]
 
     # directed graph
     def add_route(self, src, dst):
         self.adj[src].append(dst)
 
     def __str__(self):
-        for i in range(self.vertices):
+        for i in range(self.v):
             print("vertex {}: ".format(i), end='')
             for j in self.adj[i]:
                 print("{}-->".format(j), end='')
