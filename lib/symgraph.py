eb14d444ec2b4ae1698bbd6038a0ba2012d933c4
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-12 22:05:27 +0530

    symgraph.py: Add symbol table for IATA and ICOA

--- lib/symgraph.py
+++ lib/symgraph.py
@@ -11,9 +11,10 @@ class SymbolGraph:
     """
     def __init__(fname, delim):
         # construct index = dictionary> airport_name:index
-        sym_tab = {}
+        sym_tab_IATA = {}
+        sym_tab_ICAO = {}
         air_dat = []
-        key = []
+
         with open(fname) as file:
             line = file.readline()       # the header
             line = file.readline()
@@ -21,15 +22,34 @@ class SymbolGraph:
             while line != None:
                 # unpack values
                 air_id, name, city, country, iata, icao, lat, lng, alt =  line[0:9]
+                name, city, country, iata, icao = name.strip("\""),  city.strip("\""), country.strip("\""), iata.strip("\""), icao.strip("\"")
+                air_id, lat, lng, alt = int(air_id), int(lat), int(lng), int(alt)
+
                 airport = Airport(name, country, lat, lng, alt, iata, icao)
-                if name not in sym_tab.keys():
-                    sym_tab[name] = index
+                if iata is not '\\N' and iata not in sym_tab_IATA.keys() and icao is not '\\N' and icao not in sym_tab_ICAO.keys():
+                    sym_tab_IATA[iata] = index
+                    sym_tab_ICAO[icao] = index
+                    air_dat.append(airport)
+                    index += 1                
+                elif if iata is not '\\N' and iata not in sym_tab_IATA.keys():
+                    sym_tab_IATA[iata] = index
+                    air_dat.append(airport)
+                    index += 1 
+                else if icao is not '\\N' and icao not in sym_tab_ICAO.keys():
+                    sym_tab_ICAO[icao] = index
                     air_dat.append(airport)
                     index += 1
+                # don't add if both IATA and ICAO are None
             
-        # construct inverted index
-        for name in sym_tab.keys():
-            key[sym_tab[name]] = name
+        # construct inverted index for IATA code
+        key_iata = [0 for index in range(len(sym_tab_IATA))]
+        for iata in sym_tab_IATA.keys():
+            key_iata[sym_tab[iata]] = iata
+
+        # construct inverted index for ICAO code
+        key_icao = [0 for index in range(len(sym_tab_ICAO))]
+        for icao in sym_tab_ICAO.keys()
+            key_icao[sym_tab_ICAO[icao]] = icao
 
         graph = Graph(len(sym_tab))
         with open(fname as )
