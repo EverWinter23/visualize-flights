1231b7c0f7286f495e7714b33f7744f33861f478
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-12 18:58:15 +0530

    airport.py: Add function to calc Great Circle Distance

--- lib/airport.py
+++ lib/airport.py
@@ -37,3 +37,27 @@ class Airport:
         print("IATA             {}".format(self.IATA))
         print("ICAO             {}".format(self.ICAO))
         return ""
+    
+        def cal_dist_from(self, loc):
+        """
+        Calculates the distance from this airport to the other airport in kilometers.
+        """
+        AVG_EARTH_RADIUS = 6.371E3
+        
+        lat1 = self.get_lat()
+        lat2 = loc.get_lat()
+
+        lng1 = self.get_lng()
+        lng2 = loc.get_lng()
+        if lat1 is None or lng1 is None or lat2 is None or lng2 is None:
+            return inf
+
+         # convert all latitudes/longitudes from decimal degrees to radians
+        lat1, lng1, lat2, lng2 = map(radians, (lat1, lng1, lat2, lng2))
+
+        # calculate haversine
+        lat = lat2 - lat1
+        lng = lng2 - lng1
+        d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lng * 0.5) ** 2
+        h = 2 * AVG_EARTH_RADIUS * asin(sqrt(d))
+        return
